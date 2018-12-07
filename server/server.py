97cf80be0e372c059120f189d116d5e50c1f076e
edoardopuggioni <puggioniedoardo@gmail.com>
2018-12-07 20:06:22 +0100

    Work in progress, commit just for saving.

--- server/server.py
+++ server/server.py
@@ -124,6 +124,8 @@ try:
     def index():
         global board, node_id
 
+        # Following building of board_display was thought to have a "good looking" way of displaying the IDs in the
+        # web-application, but we need to keep the "ugly" IDs to maintain them unique.
         # board_display = {}
         # i = 0
         # for key in sorted(board.iteritems()):
@@ -138,6 +140,8 @@ try:
         global board, node_id
         print board
 
+        # Following building of board_display was thought to have a "good looking" way of displaying the IDs in the
+        # web-application, but we need to keep the "ugly" IDs to maintain them unique.
         # board_display = {}
         # i = 0
         # for key in sorted(board.iteritems()):
@@ -164,14 +168,14 @@ try:
             # Increment clock before event
             clock += 1
 
-            # Build stamp which will serve both as a key for the board dictionary.
-            stamp = str(clock) + str(node_id)
+            # Build entry ID which will serve as a key for the board dictionary.
+            element_id = str(clock) + str(node_id)
 
             # We add new element to dictionary using id as entry sequence.
-            add_new_element_to_store(str(stamp), new_entry)
+            add_new_element_to_store(str(element_id), new_entry)
 
             # Build path to propagate, using key word "add" and id as element_id.
-            path = "/propagate/add/" + str(clock) + '/' + str(node_id)
+            path = "/propagate/add/" + str(clock) + '/' + str(element_id)
 
             # Start thread so the server doesn't make the client wait.
             thread = Thread(target=propagate_to_vessels, args=(path, new_entry,))
@@ -187,6 +191,8 @@ try:
     @app.post('/board/<element_id:int>/')
     def client_action_received(element_id):
 
+        global clock
+
         # Modify or delete an element in the board
         # Called directly when a user is doing a POST request on /board/<element_id:int>/
 
@@ -197,11 +203,17 @@ try:
 
         if delete == "0":
             # User wants to modify entry with ID given by element_id.
+
             new_entry = request.forms.get('entry')
+
+            # Increment clock before event
+            clock += 1
+
             modify_element_in_store(str(element_id), new_entry)
 
             # Build path to propagate using keyword "mod" which stands for "modify".
-            path = "/propagate/mod/" + str(element_id)
+            # Send the new timestamp, but don't change the message ID, send the original.
+            path = "/propagate/mod/" + str(clock) + '/' + str(element_id)
 
             thread = Thread(target=propagate_to_vessels, args=(path, new_entry,))
             thread.deamon = True
@@ -209,10 +221,15 @@ try:
 
         elif delete == "1":
             # User wants to delete entry with ID given by element_id.
+
+            # Increment clock before event
+            clock += 1
+
             delete_element_from_store(entry_sequence=str(element_id))
 
             # Build path to propagate using keyword "del" which stands for "delete".
-            path = "/propagate/del/" + str(element_id)
+            # Send the new timestamp, but don't change the message ID, send the original.
+            path = "/propagate/del/" + str(clock) + '/' + str(element_id)
 
             thread = Thread(target=propagate_to_vessels, args=(path, "nothing",))
             thread.deamon = True
@@ -234,21 +251,18 @@ try:
             clock = int(msg_timestamp)
         clock += 1
 
-        new_stamp = str(msg_timestamp) + str(msg_id)
-
         if action == "add":
             # We retrieve the new entry from the body of the POST request.
             entry = request.body.read()
-            print "ADDING AN ELEMENT TO THE BOARD"
-            add_new_element_to_store(new_stamp, entry)
+            add_new_element_to_store(msg_id, entry)
 
         if action == "mod":
             # We retrieve the new entry from the body of the POST request.
             entry = request.body.read()
-            modify_element_in_store(new_stamp, entry)
+            modify_element_in_store(msg_id, entry)
 
         if action == "del":
-            delete_element_from_store(entry_sequence=new_stamp)
+            delete_element_from_store(entry_sequence=msg_idS)
 
         pass
 
